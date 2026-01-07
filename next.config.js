e9cd1024a8047e08471040e9c77b4a63ac7c1b9e
safahasogluu <hasoglusafa6@gmail.com>
2026-01-07 19:48:38 +0300

    kanonik kontrolü ve 301 eklemeleri

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/tr/transfer/asr-urgup',
+        destination: '/tr/transfer/urgup-asr-transfer',
+        permanent: true,
+      },
+      {
+        source: '/tr/transfer/asr-goreme',
+        destination: '/tr/transfer/goreme-asr-transfer',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
